2433430e1ab8f6960bb74b04db500a92bb5ecfdb
hugosenari <hugosenari@gmail.com>
2022-03-23 22:35:00 -0300

    feat(nimpkgs): remove lock

--- v0_1_16/flake.nix
+++ v0_1_16/flake.nix
@@ -13,6 +13,22 @@
   inputs.src-ginger-v0_1_16.repo  = "ginger";
   inputs.src-ginger-v0_1_16.type  = "github";
   
+  inputs."chroma".owner = "nim-nix-pkgs";
+  inputs."chroma".ref   = "master";
+  inputs."chroma".repo  = "chroma";
+  inputs."chroma".dir   = "0_2_5";
+  inputs."chroma".type  = "github";
+  inputs."chroma".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chroma".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."cairo".owner = "nim-nix-pkgs";
+  inputs."cairo".ref   = "master";
+  inputs."cairo".repo  = "cairo";
+  inputs."cairo".dir   = "1_1_1";
+  inputs."cairo".type  = "github";
+  inputs."cairo".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cairo".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
